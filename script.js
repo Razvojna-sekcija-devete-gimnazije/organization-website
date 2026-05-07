5229f77ff9fb1438b442bc91adf8288914700d99
Matija Marinković <bombmatija@gmail.com>
2026-05-07 19:43:55 +0200

    Dodat hamburger navigacioni meni na svaku stranu.

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+
+    menuToggle.classList.toggle('active');
+    navLinks.classList.toggle('active');
+
+});
+
+/* Zatvaranje menija kada klikneš link */
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+
+    link.addEventListener('click', () => {
+
+        menuToggle.classList.remove('active');
+        navLinks.classList.remove('active');
+
+    });
+
+});
