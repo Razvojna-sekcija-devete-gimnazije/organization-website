7a951604b4230cfb178eaf12f4d1bdff227c1463
Marko <markoctr2008@gmail.com>
2026-05-28 17:48:44 +0200

    dodato je kretanje

--- igrice/marko/skripta.js
+++ igrice/marko/skripta.js
@@ -1,9 +1,15 @@
 //Define HTML elements
 const board=document.getElementById('game-board');
+const instructions=document.getElementById('instruction-text');
+
 
 //Define game variables
 let snake=[{x:10,y:10}];
 let food= generateFood();
+let direction='right';
+let gameInterval;
+let gameSpeed=200;
+let gameStarted=false;
 
 //Draw game map,snake,food
 function draw(){
@@ -34,9 +40,6 @@ function setPosition(element,position){
     element.style.gridRow=position.y;
 }
 
-//Testing the function
-draw();
-
 //drawFood
 function drawFood(){
     const foodElement=createGameElement('div','food');
@@ -44,8 +47,77 @@ function drawFood(){
     board.appendChild(foodElement);
 }
 
+//Generate food
 function generateFood(){
     const x=Math.floor(Math.random()*20)+1;
     const y=Math.floor(Math.random()*20)+1;
     return{x,y};
 }
+
+//Moving the snake
+function moveSnake(){
+    const head={...snake[0]};
+    switch(direction){
+        case'right':
+            head.x++;
+            break;
+        case'left':
+            head.x--;
+            break;
+        case'up':
+            head.y--;
+            break;
+        case'down':
+            head.y++;
+            break;
+    }
+    snake.unshift(head);
+    if(head.x==food.x && head.y==food.y){
+        food=generateFood();
+        clearInterval(gameInterval);
+        gameInterval=setInterval(()=>{
+            moveSnake();
+            //checkCollision();
+            draw();
+        },gameSpeed);
+    }
+    else snake.pop();
+}
+
+//start game
+function startGame(){
+    gameStarted=true;
+    instructions.style.display='none';
+    gameInterval=setInterval(()=>{
+        moveSnake();
+        //checkCollision();
+        draw();
+    },gameSpeed)
+}
+
+//keypress event listener
+function handleKeyPress(event){
+    if((!gameStarted && event.code==='Space')||
+    (!gameStarted && event.key===' ') ) {startGame();}
+    else{
+        switch(event.key){
+            case'ArrowUp':
+            if(direction!=='down')
+            direction='up';
+            break;
+            case'ArrowDown':
+            if(direction!=='up')
+            direction='down';
+            break;
+            case'ArrowLeft':
+            if(direction!=='right')
+            direction='left';
+            break;
+            case'ArrowRight':
+            if(direction!=='left')
+            direction='right';
+            break;
+        }
+    }
+}
+document.addEventListener('keydown',handleKeyPress);
