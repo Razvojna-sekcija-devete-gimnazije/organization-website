a3708ee5c748130286da904d118609092f99016a
Marko <markoctr2008@gmail.com>
2026-05-28 16:51:54 +0200

    generisanje zmije i hrane na pocetku

---
+++ igrice/marko/skripta.js
@@ -0,0 +1,51 @@
+//Define HTML elements
+const board=document.getElementById('game-board');
+
+//Define game variables
+let snake=[{x:10,y:10}];
+let food= generateFood();
+
+//Draw game map,snake,food
+function draw(){
+    board.innerHTML='';
+    drawSnake();
+    drawFood();
+}
+
+//Draw snake
+function drawSnake(){
+    snake.forEach((segment)=>{
+        const snakeElement=createGameElement('div','snake');
+        setPosition(snakeElement,segment);
+        board.appendChild(snakeElement);
+    });
+}
+
+//Create a snake or food cubr/div
+function createGameElement(tag,className){
+    const element=document.createElement(tag);
+    element.className=className;
+    return element;
+}
+
+//Set position of snake or food
+function setPosition(element,position){
+    element.style.gridColumn=position.x;
+    element.style.gridRow=position.y;
+}
+
+//Testing the function
+draw();
+
+//drawFood
+function drawFood(){
+    const foodElement=createGameElement('div','food');
+    setPosition(foodElement,food);
+    board.appendChild(foodElement);
+}
+
+function generateFood(){
+    const x=Math.floor(Math.random()*20)+1;
+    const y=Math.floor(Math.random()*20)+1;
+    return{x,y};
+}
